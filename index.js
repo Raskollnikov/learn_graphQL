c1e4fe2673c9ac12405e5501d1009449f979754b
Raskollnikov <bakhalaia56@gmail.com>
2025-06-03 01:23:40 +0400

    suiiii amazing

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const userData  = require('./MOCK_DATA.json');
-
 const app = express();
-const PORT = 3000;
+const PORT = 3000; 
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./Schemas/index.js') 
 
-app.get('/users', (req, res) => {
-    res.json(userData); 
-});
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true 
+}));
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
